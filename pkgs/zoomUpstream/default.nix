94a9b23bba07c88d27c5303344617c74b75f5923
Inayet Hadi <inayet@dreamsapi.com>
2025-09-19 21:48:29 -0600

    Fix runtime: add libgbm to LD_LIBRARY_PATH

--- pkgs/zoomUpstream/default.nix
+++ pkgs/zoomUpstream/default.nix
@@ -156,7 +156,7 @@ stdenv.mkDerivation rec {
     # Precompute PATH and XDG_DATA_DIRS prefixes
     pathPrefix=${lib.makeBinPath [ pulseaudio xdg-utils dbus xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr pipewire wireplumber ]}
     xdgDataDirs=${lib.makeSearchPath "share" [ xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr ]}
-    runtimeLibs="${lib.makeLibraryPath [ glib gtk3 gdk-pixbuf pango cairo freetype fontconfig libdrm libglvnd alsa-lib pulseaudio libpulseaudio nspr nss wayland libxkbcommon pipewire udev systemd xorg.libX11 xorg.libxcb xorg.libXext xorg.libXrender xorg.libXcomposite xorg.libXcursor xorg.libXdamage xorg.libXfixes xorg.libXi xorg.libXrandr xorg.libXtst xorg.libXScrnSaver xorg.libxshmfence xorg.xcbutil xorg.xcbutilcursor xorg.xcbutilimage xorg.xcbutilwm xorg.xcbutilrenderutil xorg.xcbutilkeysyms libuuid mesa atk at-spi2-atk at-spi2-core expat dbus ]}"
+    runtimeLibs="${lib.makeLibraryPath [ glib gtk3 gdk-pixbuf pango cairo freetype fontconfig libdrm pkgs.libgbm libglvnd alsa-lib pulseaudio libpulseaudio nspr nss wayland libxkbcommon pipewire udev systemd xorg.libX11 xorg.libxcb xorg.libXext xorg.libXrender xorg.libXcomposite xorg.libXcursor xorg.libXdamage xorg.libXfixes xorg.libXi xorg.libXrandr xorg.libXtst xorg.libXScrnSaver xorg.libxshmfence xorg.xcbutil xorg.xcbutilcursor xorg.xcbutilimage xorg.xcbutilwm xorg.xcbutilrenderutil xorg.xcbutilkeysyms libuuid mesa atk at-spi2-atk at-spi2-core expat dbus ]}"
 
     # Move real binaries and remove symlink
     mv -f $out/zoom/zoom $out/zoom/zoom.real
